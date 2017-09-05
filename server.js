0c8be98af10f67459cd9bf521612f4a0413cc8a7
Martin Villalba <14tinchov@gmail.com>
2017-09-05 15:48:36 -0300

    Set prerender

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 var prerender = require('./lib');
 
 var server = prerender({
-    workers: process.env.PRERENDER_NUM_WORKERS,
-    iterations: process.env.PRERENDER_NUM_ITERATIONS
+    workers: 3,
+    iterations: process.env.PRERENDER_NUM_ITERATIONS,
+    logRequests: true,
+    pageDoneCheckTimeout: 9000,
+    resourceDownloadTimeout: 50000
 });
 
 
@@ -11,9 +14,10 @@ server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
 server.use(prerender.blacklist());
-// server.use(prerender.logger());
+server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
-server.use(prerender.httpHeaders());
+server.use(require('prerender-redis-cache'));
+// server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
 
