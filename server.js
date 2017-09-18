dba58aaac04c28864b8afc9d9efed071178c9f99
Martin Villalba <14tinchov@gmail.com>
2017-09-18 15:55:37 -0300

    Chane workers

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var prerender = require('./lib');
 
 var server = prerender({
-    workers: 4,
+    workers: 3,
     iterations: process.env.PRERENDER_NUM_ITERATIONS,
     logRequests: true,
     pageDoneCheckTimeout: 9000,
